96552f3dd2305cc148ad34df276d6706e1602d6a
Jorge Márquez <Jorge Mrquez>
2016-07-24 19:53:41 +0200

    user encryptation

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var multiparty = require('multiparty');
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
 var User = require('../models/user');
 
 /* GET users listing. */
@@ -99,6 +103,45 @@ router.get('/login', function(req, res, next) {
   res.render('users/login', {
   	'title': 'Login'
   });
+});
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.getUserById(id, function(err, user){
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    User.getUserByUsername(username, function(err, user){
+      if(err) throw err;
+      if(!user){
+        console.log('Unknown user');
+        return done(null, false, {message: 'Unknown User'});
+      }
+
+      User.comparePassword(password, user.password, function(err, isMatch){
+        console.log('comparing passwords');
+        if(err) throw err;
+        if(isMatch){
+          return done(null, user);
+        } else{
+          console.log('Invalid Password');
+          return done(null, false, {message:'Invalid Password'});
+        }
+      });
+    })
+  }
+))
+
+router.post('/login', passport.authenticate('local', {failureRedirect: '/users/login', failureflash: 'Invalid username or password'}), function(req, res){
+  console.log('Authentication Successful');
+  req.flash('success', 'You are logged in');
+  res.redirect('/members')
 });
 
 router.get('/logout', function(req, res, next) {
